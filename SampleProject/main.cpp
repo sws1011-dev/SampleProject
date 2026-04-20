603b46ef4e275046832b331ec146f7c68b1a7f97
wooseok <sws1011.dev@gmail.com>
2026-04-20 15:24:53 +0900

    feat: 변수의 기본 연산자 동작 확인

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -8,10 +8,6 @@ int main()
 	char charactorClass[50];
 
 	// 1. 다양한 자료형의 변수 선언 및 초기값 할당
-	int level = 1;
-	int hp = 100;
-	int mp = 100;
-	float attackSpeed = 5.5f;
 
 	// 스탯 시스템
 	int strength = 50;
@@ -19,13 +15,19 @@ int main()
 	int vitality = 50;
 	int energy = 50;
 
-	// 하드코어 모드 여부 변수
-	bool isHardcore = true;
-
+	int level = 1;
+	int hp = vitality * 2;
+	int mp = energy * 1.5;
+	float attackDamage = strength * 0.2f;
+	float attackSpeed = dexterity / 10.0f;
+	double movingSpeed = dexterity / 30.0;
 	int fireResist = 0;
 	int lightningResist = 0;
 	int coldResist = 0;
 	int poisonResist = 0;
+
+	// 하드코어 모드 여부 변수
+	bool isHardcore = true;
 
 	cout << "[Character Creation]\n";
 	cout << "Input your name : ";
@@ -41,7 +43,9 @@ int main()
 	cout << "Level : " << level << "\n";
 	cout << "HP : " << hp << "\n";
 	cout << "MP : " << mp << "\n";
+	cout << "Attack Damage: " << attackDamage << "\n";
 	cout << "Attack Speed : " << attackSpeed << "\n";
+	cout << "Moving Speed : " << movingSpeed << "\n";
 	cout << "Strength : " << strength << "\n";
 	cout << "Dexterity : " << dexterity << "\n";
 	cout << "Vitality : " << vitality << "\n";
@@ -56,10 +60,13 @@ int main()
 
 	cout << "-----------------------------------------------------\n";
 
-
 	// 3. sizeof 연산자를 이용한 메모리 크기 확인
 	cout << "[Memory Check] int type size : " << sizeof(hp) << "bytes\n";
 	cout << "[Memory Check] bool type size : " << sizeof(isHardcore) << "bytes\n";
+	cout << "[Memory Check] float type size : " << sizeof(float) << "bytes\n";
+	cout << "[Memory Check] float type size : " << sizeof(double) << "bytes\n";
+	cout << "[Memory Check] char type size : " << sizeof(char) << "bytes\n";
+	cout << "[Memory Check] char array(userName) size : " << sizeof(userName) << "bytes\n";
 
 	return 0;
 }
