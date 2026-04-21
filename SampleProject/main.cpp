7c0c33052824fd3807d6977eec40f50277d7ecdf
wooseok <sws1011.dev@gmail.com>
2026-04-21 15:32:46 +0900

    feat: delete를 통한 포인터 메모리 해제 시, nullptr을 통한 안전처

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -97,6 +97,14 @@ int main()
 
 	cout << "wildPtr : " << wildPtr << "\n"; // 0
 
+	// 허상 포인터 예시
+	int* danglePtr = new int(100); // 동적 할당
+	cout << "삭제(delete) 전 : " << *danglePtr << "\n"; // 100
+	delete danglePtr;		// 메모리 해제, 삭제
+	// *danglePtr = 200;	// 해제된 메모리에 할당하려 하고 있음. Runtime CRASH 발생
+	danglePtr = nullptr;	// 안전처리
+	cout << "삭제(delete) 후 : " << danglePtr << "\n"; // ??
+
 	system("pause");
 
 	// [UI] 타이틀 화면 (아스키 아트 추가)
