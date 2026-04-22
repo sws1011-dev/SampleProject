b466a7883c9e8b2713d1219d6fdabadfc95e93fa
wooseok <sws1011.dev@gmail.com>
2026-04-22 14:26:23 +0900

    feat: Call By Reference- 참조자에 의한 호출2
    - 함수의 매개변수 전달 방식으로 참조자 초기화 동시 진행

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -16,6 +16,11 @@ void LevelUp(int* level) {
 	(*level)++;	// 역참조로 level 원본 직접 증가
 }
 
+// Call By Reference: 참조자 전달 -> * 없이 직접 수정
+void LevelUpRef(int& level) {
+	level++;
+}
+
 int main()
 {
 	char userName[50];
@@ -49,10 +54,18 @@ int main()
 	cout << "레벨업 후 level: " << level << "\n";
 
 	// Call By Reference: 별칭(Alias) 선언 -> 원본과 같은 메모리
-	int& levelRef = level;	// level의 별칭 선언
-	levelRef++;	// levelRef 수정 -> level이 수정될 것임
-	cout << "levelRef++ 후 원본 level: " << level << "\n";
-	cout << "levelRef++과 level이 동일한 값?: " << levelRef << "\n";
+	//int& levelRef = level;	// level의 별칭 선언
+	//levelRef++;	// levelRef 수정 -> level이 수정될 것임
+	//cout << "levelRef++ 후 원본 level: " << level << "\n";
+	//cout << "levelRef++과 level이 동일한 값?: " << levelRef << "\n";
+
+	// Call By Reference: & 없이 호출, * 없이 수정
+	cout << "levelUpRef() 호출 전 원본 level: " << level << "\n";
+	LevelUpRef(level); // & 없이 그냥 변수명
+	cout << "levelUpRef() 호출 후 원본 level: " << level << "\n";
+
+
+
 
 
 	/**
